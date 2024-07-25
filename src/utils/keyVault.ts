5a35909e4499ca939e5aab1845c745835b987349
Holmberg18 <holmberg18@gmail.com>
2024-07-24 23:07:18 -0700

    [+/-] Update getApiKey return type

--- src/utils/keyVault.ts
+++ src/utils/keyVault.ts
@@ -1,4 +1,4 @@
-export const getApiKey = async ():Promise<string | undefined> => {
+export const getApiKey = (): string | undefined => {
 
     if(import.meta.env.VITE_REACT_ENV === "Development"){
         return import.meta.env.VITE_API_KEY
