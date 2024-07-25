bfa156df49273e65482caeda15962ad4b9492046
Holmberg18 <holmberg18@gmail.com>
2024-07-24 22:19:45 -0700

    [+/-] Add api prod key secret key azure identifier, update keyVault utility function

--- src/utils/keyVault.ts
+++ src/utils/keyVault.ts
@@ -1,23 +1,9 @@
-import { KeyVaultSecret, SecretClient } from "@azure/keyvault-secrets";
-import { DefaultAzureCredential } from "@azure/identity";
-
 export const getApiKey = async ():Promise<string | undefined> => {
 
     if(import.meta.env.VITE_REACT_ENV === "Development"){
         return import.meta.env.VITE_API_KEY
     }
-    
-    const keyVaultName: string | undefined = import.meta.env.VITE_KEY_VAULT_NAME;
-    if(!keyVaultName) throw new Error("KEY_VAULT_NAME is empty");
-    const url: string = "https://" + keyVaultName + ".vault.azure.net";
-    const secretClient: SecretClient = new SecretClient(url, new DefaultAzureCredential())
-    //Grab api key from key vault
-    try {
-        const secret: KeyVaultSecret = await secretClient.getSecret("spice-delight-app-api-key");
-        return secret.value;
-    } catch (error: any) {
-        console.error("Error fetching Key Vault secret:", error.message);
-        throw error;
-    }
+
+    return import.meta.env.VITE_API_PROD_KEY
     
 }
