ac26db8d4aca5c63fdfe851f7b048e8a2b8e3870
Holmberg18 <holmberg18@gmail.com>
2024-08-09 16:52:20 -0700

    [+/-] Move frontend files to client folder

--- src/models/Customer.ts
+++
@@ -1,9 +0,0 @@
-export interface  Customer {
-    customerId: string,
-    firstName: string,
-    lastName: string,
-    email: string,
-    phone: string,
-    address: string,
-    username: string
-}
--- src/utils/stripe.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const getStripeKey = async (keyName: string): Promise<StripeKey | void> => {
-    return axios.get(`/api/stripe/${keyName}`)
-        .then((response: any) => response.data)
-        .catch((error) => {
-            console.log(error);
-        });
-};
