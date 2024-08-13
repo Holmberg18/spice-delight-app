289bb91e6efa707a4d1c75d11d8cbae6a423dc7e
Holmberg18 <holmberg18@gmail.com>
2024-08-12 23:24:42 -0700

    [+/-] Add environment variables for client, change port to 8080

--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL || 'http://localhost:5000',
+    baseURL: import.meta.env.VITE_ENV === "Development" ? 'http://localhost:8080' : import.meta.env.VITE_AZURE_BASE_URL,
     headers: {
         "Content-Type": "application/json"
     }
--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors(corsOptions))
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const distPath = path.join(__dirname, "../client/dist")
 app.use(express.static(distPath))
 
