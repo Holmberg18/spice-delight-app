5b648e1c850bf0a63a92d7f24eaac218623606d4
Holmberg18 <holmberg18@gmail.com>
2024-08-15 00:40:46 -0700

    [+/-] Reset base url to default for axiosInstance

--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_ENV === "Development" ? 'http://localhost:7071' : import.meta.env.VITE_AZURE_BASE_URL,
+    baseURL: 'http://localhost:7071',
     headers: {
         "Content-Type": "application/json"
     }
