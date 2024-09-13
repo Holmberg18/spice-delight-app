2990e06f75f563fe2c24fdce84fb20af7193c6d9
Holmberg18 <holmberg18@gmail.com>
2024-09-12 21:31:34 -0700

    [+/-] Update deploy file to run unit tests, exclude integration tests

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,14 +1,18 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 import path from "path"
 
 export default defineConfig({
     test: {
         globals: true,
         environment: 'jsdom',
-        setupFiles: "src/tests/setup.ts"
+        setupFiles: "src/tests/setup.ts",
+        exclude:[
+          ...configDefaults.exclude, 
+          'integration/*'
+        ]
     },
     resolve: {
         alias: {
