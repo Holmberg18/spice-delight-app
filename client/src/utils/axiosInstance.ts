89f5199ddac10b01d71a59a3438caa7395e6047c
Holmberg18 <holmberg18@gmail.com>
2024-09-20 23:28:06 -0700

    [+/-] Add Vite env variables, configure azure deployment to set up env variables and base url

--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:7071',
+    baseURL: import.meta.env.VITE_AZURE_BASE_URL,
     headers: {
         "Content-Type": "application/json"
     }
