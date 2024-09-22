5a9d4fbaf80e110b7a0442a038efed7349f36ba6
Holmberg18 <holmberg18@gmail.com>
2024-09-21 22:56:53 -0700

    [+/-] Update base url for production

--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 // src/axiosInstance.ts
 import axios from 'axios';
 
-const url: string = import.meta.env.VITE_AZURE_BASE_URL || "/"
+const url: string = import.meta.env.VITE_AZURE_BASE_URL || ""
 
 const axiosInstance = axios.create({
     baseURL: url,
